5c2aeea76c18110ffa8fd57480ae69bc82f8ac03
yokoyama.ryosuke <indexbraker02@yahoo.co.jp>
2021-02-11 22:56:32 +0900

    [add] Pentagon Drawing

---
+++ p5js/Pentagon/Pentagon.js
@@ -0,0 +1,83 @@
+function setup () {
+  createCanvas(windowWidth, windowHeight)
+  frameRate(20)
+  background(30)
+  smooth()
+  p = new Pentagon()
+}
+
+function draw () {
+  p.display()
+}
+
+
+class Pentagon {
+
+  constructor() {
+    this.rotateValue = 4
+    this.rotate = []
+    this.radius = 300
+    this.pointValue = 5
+    this.angle = []
+    this.crdnt = []
+    this.dp = []
+    this.vec = []
+    this.mdp = []
+
+    for (var i = 0; i < this.rotateValue; i++) {
+      this.rotate.push(TWO_PI / this.rotateValue * i)
+    }
+
+    for (var i = 0; i < this.pointValue; i++) {
+      this.angle.push(TWO_PI / this.pointValue * i)
+      this.crdnt.push([this.radius * cos(this.angle[i]), this.radius * sin(this.angle[i])])
+    }
+  }
+
+  display () {
+    translate(width / 2, height / 2)
+    let i = frameCount % this.rotateValue
+    rotate(this.rotate[i])
+    // this.printPoint()
+
+    if (i % 2) {
+      stroke(255, 2)
+      strokeWeight(9)
+    } else {
+      stroke(255, 30)
+      strokeWeight(6)
+    }
+    this.printLine()
+
+    if(frameCount > this.rotateValue * 20) {
+      noLoop()
+    }
+  }
+
+  printPoint () {
+    stroke(255)
+    strokeWeight(5)
+
+    for (var k = 0; k < this.crdnt.length; k++) {
+      point(this.crdnt[k][0], this.crdnt[k][1])
+    }
+  }
+
+  printLine () {
+    for (var i = 0; i < this.crdnt.length; i++) {
+      for (var j = i + 1; j < this.crdnt.length; j++) {
+        line(this.crdnt[i][0], this.crdnt[i][1], this.crdnt[j][0], this.crdnt[j][1])
+
+      }
+    }
+  }
+}
+
+function windowResized () {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseClicked(){
+  noLoop()
+  saveFrames("Pentagon", 'png', 1, 1);
+}

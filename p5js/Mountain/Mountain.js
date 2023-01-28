fa3fd8b2c8f40079808c03e352a3546c8803501d
yokoyamark <yokoyamark860@gmail.com>
2023-01-29 01:23:40 +0900

    lotus

--- p5js/Mountain/Mountain.js
+++ p5js/Mountain/Mountain.js
@@ -1,7 +1,7 @@
 function setup () {
   createCanvas(windowWidth, windowHeight)
-  frameRate(30)
-  background(30)
+  frameRate(60)
+  background(0)
   smooth()
   p = new Mountain()
 }
@@ -23,27 +23,33 @@ class Mountain {
     if (f == 0) {
       return
     }
-
     if (this.plus == 0.0) {
       this.plus = TWO_PI / (f * 10)
       console.log("FrameRate: " + f)
     }
+    // 画面中央を基準点に
     translate(width / 2, height / 2)
-    rotate(HALF_PI * 3)
-    let len = 200
+    // x,y 座標をユークリッド空間
+    // rotate(HALF_PI * 3)
+
+    let len = 160
     stroke(255)
-    strokeWeight(10)
+    strokeWeight(5)
     point(len * cos(this.radians), len * sin(this.radians))
+
+    let divide = 8
+    for(let i = 0; i < divide; i++) {
+      point(len * tan(this.radians), len * sin(this.radians))
+      rotate(PI / divide)
+    }
 
     if (this.radians < TWO_PI) {
       console.log(this.plus)
       this.radians += this.plus
       console.log(this.radians)
+    } else {
+      noLoop()
     }
-
-    // if (this.radians < PI) {
-      point(len * tan(this.radians), len * sin(this.radians))
-    // }
 
   }
 }
@@ -52,7 +58,7 @@ function windowResized () {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function mouseClicked(){
+function mouseClicked () {
   noLoop()
   // saveFrames("Mountain", 'png', 1, 1);
 }
